6ffc7b46694d1ad87ee04c8c0bc200ae89f60ba5
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2023-11-26 14:48:45 -0500

    all data headers removed and formatted correctly

--- api/dataColumnRemover.py
+++ api/dataColumnRemover.py
@@ -10,25 +10,24 @@ weeklyPlayerGameLogsPath = 'C:/Users/samue/PycharmProjects/2023_NFL_ML_Projects/
 
 def cleanDataHeaders(dirpath, season=None, week=None):
     dotenv.load_dotenv()
-    currentSeasonYear = os.getenv('CURRENT_SEASON_YEAR')
-    lastCompletedWeek = os.getenv('CURRENT_SEASON_LAST_COMPLETED_WEEK')
+    currentSeasonYear = int(os.getenv('CURRENT_SEASON_YEAR'))
+    lastCompletedWeek = int(os.getenv('CURRENT_SEASON_LAST_COMPLETED_WEEK'))
 
     for subdir in os.listdir(dirpath):
         subdirPath = dirpath + subdir + '/'
-        subdirSeason = subdir
+        subdirSeason = int(subdir)
         for file in os.listdir(subdirPath):
             filePath = subdirPath + file
-            fileWeek = file[:-4]
+            fileWeek = int(file[:-4])
             if subdirSeason == currentSeasonYear and fileWeek > lastCompletedWeek:
                 continue
             df = pd.read_csv(filePath)
             df = df.iloc[: , 1:]
-            pd.DataFrame(df).to_csv("C:/Users/samue/PycharmProjects/2023_NFL_ML_Projects/tmp/XD.csv", index=False)
-
+            pd.DataFrame(df).to_csv(filePath, index=False)
 
 
 # df = pd.read_csv('C:/Users/samue/PycharmProjects/2023_NFL_ML_Projects/data/nfl/weekly_team_game_logs/2017/1.csv')
 # df = df.iloc[: , 1:]
 # pd.DataFrame(df).to_csv("C:/Users/samue/PycharmProjects/2023_NFL_ML_Projects/tmp/XD.csv", index=False)
 
-cleanDataHeaders(weeklyTeamGameLogsPath)
+cleanDataHeaders(weeklyPlayerGameLogsPath)
